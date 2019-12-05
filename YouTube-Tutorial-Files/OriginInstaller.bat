3ffee78d6a94d539d9ff040ab1c95f5c579e4b51
Mike Galvin <digressive@outlook.com>
2019-12-05 16:19:48 +0000

    Forgot the OriginInstall.bat file!

---
+++ YouTube-Tutorial-Files/OriginInstaller.bat
@@ -0,0 +1,7 @@
+echo off
+Z:\Applications\Origin\originsetup.exe /silent
+:loop
+timeout /t 10 /nobreak
+tasklist /fi "imagename eq originthinsetupinternal.exe" |find ":" > nul
+if errorlevel 1 goto loop
+exit

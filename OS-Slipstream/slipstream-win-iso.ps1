6f0b7945bbd4b1d55864480a6ed186076045c597
Digressive <digressive@outlook.com>
2024-03-18 15:34:41 +0000

    Update slipstream-win-iso.ps1

--- OS-Slipstream/slipstream-win-iso.ps1
+++ OS-Slipstream/slipstream-win-iso.ps1
@@ -16,10 +16,10 @@ Write-Host -Object ""
 ## Only used is makeISO is configured
 If ($MakeIso -eq "enabled")
 {
-    $OSCLoc = "X:\Mike\OneDrive\apps\Oscdimg" #C:\Program Files (x86)\Windows Kits\10\Assessment and Deployment Kit\Deployment Tools\amd64\Oscdimg ##default location when ADK is installed
+    $OSCLoc = "C:\scripts\Oscdimg" #C:\Program Files (x86)\Windows Kits\10\Assessment and Deployment Kit\Deployment Tools\amd64\Oscdimg ##default location when ADK is installed
     If ((Test-Path -Path "$OSCLoc\oscdimg.exe") -eq $false)
     {
-        Write-Host -Object "  oscdimg.exe not found. I need it to make an iso file."
+        Write-Host -Object " oscdimg.exe not found. I need it to make an iso file."
         exit
     }
 }
@@ -35,7 +35,7 @@ If (Test-Path -Path "$PSScriptRoot\$IsoSrc")
         If (Test-Path -Path $IsoPath)
         {
             ## Copy Source Files
-            Write-Host -Object "  Copying Windows install source files"
+            Write-Host -Object " Copying Windows install source files"
             Mount-DiskImage -ImagePath "$IsoPath" -NoDriveLetter | Out-Null
             $IsoPathMnt = Get-DiskImage "$IsoPath" | Select-Object DevicePath -ExpandProperty DevicePath
             Copy-Item -Path "$IsoPathMnt\" -Destination "$PSScriptRoot\$IsoName-src" -Recurse
@@ -60,13 +60,13 @@ If (Test-Path -Path "$PSScriptRoot\$IsoSrc")
 
                 ForEach ($OrigImgIndex in $EnumOGImgIndexes)
                 {
-                    Write-Host -Object "  Converting index $($OrigImgIndex.ImageIndex) of $($EnumOGImgIndexes.ImageIndex.count): $($OrigImgIndex.ImageName)"
+                    Write-Host -Object " Converting index $($OrigImgIndex.ImageIndex) of $($EnumOGImgIndexes.ImageIndex.count): $($OrigImgIndex.ImageName)"
                     Export-WindowsImage -SourceImagePath "$PSScriptRoot\$IsoName-src\sources\$OrigImgName" -SourceIndex $($OrigImgIndex.ImageIndex) -DestinationImagePath "$PSScriptRoot\$IsoName-src\sources\$DecomImgName" -CompressionType maximum | Out-Null
                 }
             }
 
             else {
-                    Write-Host -Object "  Converting index $SetIndex"
+                    Write-Host -Object " Converting index $SetIndex"
                     Export-WindowsImage -SourceImagePath "$PSScriptRoot\$IsoName-src\sources\$OrigImgName" -SourceIndex $SetIndex -DestinationImagePath "$PSScriptRoot\$IsoName-src\sources\$DecomImgName" -CompressionType maximum | Out-Null
             }
 
@@ -89,14 +89,14 @@ If (Test-Path -Path "$PSScriptRoot\$IsoSrc")
                     ## Apply Updates
                     If (Test-Path -Path "$PSScriptRoot\$UpdatesSrc")
                     {
-                        Write-Host -Object "  Adding updates to index $($WimIndex.ImageIndex) of $($EnumIndexes.ImageIndex.count): $($WimIndex.ImageName)"
+                        Write-Host -Object " Adding updates to index $($WimIndex.ImageIndex) of $($EnumIndexes.ImageIndex.count): $($WimIndex.ImageName)"
                         Add-WindowsPackage -Path "$PSScriptRoot\win-wim$($WimIndex.ImageIndex)-mnt" -PackagePath "$PSScriptRoot\$UpdatesSrc" -IgnoreCheck -ErrorAction SilentlyContinue
                     }
 
                     ## Add drivers
                     If (Test-Path -Path "$PSScriptRoot\$DriversSrc")
                     {
-                        Write-Host -Object "  Adding drivers to index $($WimIndex.ImageIndex) of $($EnumIndexes.ImageIndex.count): $($WimIndex.ImageName)"
+                        Write-Host -Object " Adding drivers to index $($WimIndex.ImageIndex) of $($EnumIndexes.ImageIndex.count): $($WimIndex.ImageName)"
                         Add-WindowsDriver -Path "$PSScriptRoot\win-wim$($WimIndex.ImageIndex)-mnt" -Driver "$PSScriptRoot\$DriversSrc" -Recurse
                     }
 
@@ -105,7 +105,7 @@ If (Test-Path -Path "$PSScriptRoot\$IsoSrc")
                     Dism.exe /Image:"$PSScriptRoot\win-wim$($WimIndex.ImageIndex)-mnt" /Cleanup-Image /RestoreHealth /StartComponentCleanup /ResetBase | Out-Null
 
                     ## Unmount Wim
-                    Write-Host -Object "  Saving changes"
+                    Write-Host -Object " Saving changes"
                     Dismount-WindowsImage -Path "$PSScriptRoot\win-wim$($WimIndex.ImageIndex)-mnt" -Save | Out-Null
                     Remove-Item -Path "$PSScriptRoot\win-wim$($WimIndex.ImageIndex)-mnt" -Recurse -Force
                 }
@@ -115,7 +115,7 @@ If (Test-Path -Path "$PSScriptRoot\$IsoSrc")
             If ($HighComp -eq "enabled")
             {
                 $EnumIndexesHC = Get-WindowsImage -ImagePath "$PSScriptRoot\$IsoName-src\sources\install.wim" | Select-Object -Property ImageName,ImageIndex
-                Write-Host -Object "  $($EnumIndexesHC.ImageIndex.count) Image Indexes to compress"
+                Write-Host -Object " $($EnumIndexesHC.ImageIndex.count) Image Indexes to compress"
 
                 ForEach ($WimIndexHC in $EnumIndexesHC)
                 {
@@ -128,21 +128,21 @@ If (Test-Path -Path "$PSScriptRoot\$IsoSrc")
             ## Make ISO
             If ($MakeISO -eq "enabled")
             {
-                Write-Host -Object "  Creating file $IsoName-updt.iso"
+                Write-Host -Object " Creating file $IsoName-updt.iso"
                 Start-Process $OSCLoc\oscdimg.exe -ArgumentList "-m -o -u2 -udfver102 -bootdata:2#p0,e,b$PSScriptRoot\$IsoName-src\boot\etfsboot.com#pEF,e,b$PSScriptRoot\$IsoName-src\efi\microsoft\boot\efisys.bin $PSScriptRoot\$IsoName-src $PSScriptRoot\$IsoName-updt.iso" -Wait
 
                 ## Clean up
-                Write-Host -Object "  Cleaning up"
+                Write-Host -Object " Cleaning up"
                 Remove-Item -Path "$PSScriptRoot\$IsoName-src" -Recurse -Force
             }
         }
 
         else {
-            Write-Host -Object "  No iso(s) found"
+            Write-Host -Object " No iso(s) found"
         }
     }
 }
 
 else {
-    Write-Host -Object "  No ISO folder found"
+    Write-Host -Object " No ISO folder found"
 }
